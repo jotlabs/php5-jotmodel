9c85e02592055994c914e1148266bff448416e29
Mike Davies <isofarro2@gmail.com>
2014-05-31 09:07:18 +0100

    Create PdoDataSource

---
+++ lib/JotModel/DataSources/Implementations/PdoDataSource.php
@@ -0,0 +1,19 @@
+<?php
+namespace JotModel\DataSources\Implementations;
+
+use PDO;
+use JotModel\DataSources\DataSource;
+
+class PdoDataSource implements DataSource
+{
+    protected $db;
+
+    public function __construct($db)
+    {
+        if (is_a($db, 'PDO')) {
+            $this->db = $db;
+        } else {
+            throw new JotModelException("PdoDataSource requires a PDO object");
+        }
+    }
+}
