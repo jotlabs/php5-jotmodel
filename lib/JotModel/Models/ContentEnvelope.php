248dc5b4ee3de5eadc5a97fe9a1eb473853d3bd6
Mike Davies <isofarro2@gmail.com>
2014-06-01 12:16:38 +0100

    ContentEnvelope pattern

---
+++ lib/JotModel/Models/ContentEnvelope.php
@@ -0,0 +1,20 @@
+<?php
+namespace JotModel\Models;
+
+class ContentEnvelope
+{
+    protected $envelopeId;
+
+    protected $status;
+    protected $model;
+
+    public $slug;
+    public $title;
+    public $excerpt;
+
+    public $permalink;
+    public $imageTemplate;
+
+    public $dateAdded;
+    public $dateUpdated;
+}
