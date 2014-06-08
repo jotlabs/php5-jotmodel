db471ec2c5af20b39ba13d6e928c9279339276c5
Mike Davies <isofarro2@gmail.com>
2014-06-08 09:01:19 +0100

    Use a view instead of a four table join

--- lib/JotModel/Models/ContentEnvelope.php
+++ lib/JotModel/Models/ContentEnvelope.php
@@ -6,29 +6,25 @@ class ContentEnvelope
     public static $MODEL = 'content';
 
     public static $SQL_QUERIES = array(
-        'getBySlug' => 'SELECT {fieldList} FROM `content` {joins} WHERE content.slug=:slug;'
+        'getBySlug' => 'SELECT {fieldList} FROM `content_envelope` WHERE slug=:slug;'
     );
 
     public static $SQL_FRAGMENTS = array(
-        'joins' => array(
-            'LEFT JOIN `content_status` ON content._status_id = content_status._id',
-            'LEFT JOIN `content_models` ON content._model_id = content_models._id',
-            'LEFT JOIN `content_types`  ON content_models._type_id = content_types._id'
-        )
+        'joins' => array()
     );
 
     public static $SQL_FIELDS = array(
-        'envelopeId'    => 'content._id',
-        'contentId'     => 'content._content_id',
-        'status'        => 'content_status.slug',
-        'model'         => 'content_models.slug',
-        'slug'          => 'content.slug',
-        'title'         => 'content.title',
-        'excerpt'       => 'content.excerpt',
-        'permalink'     => 'content.permalink',
-        'imageTemplate' => 'content.imageTemplate',
-        'dateAdded'     => 'content.dateAdded',
-        'dateUpdated'   => 'content.dateUpdated'
+        'envelopeId'    => '_envelope_id',
+        'contentId'     => '_content_id',
+        'status'        => 'status',
+        'model'         => 'model',
+        'slug'          => 'slug',
+        'title'         => 'title',
+        'excerpt'       => 'excerpt',
+        'permalink'     => 'permalink',
+        'imageTemplate' => 'imageTemplate',
+        'dateAdded'     => 'dateAdded',
+        'dateUpdated'   => 'dateUpdated'
     );
 
 
