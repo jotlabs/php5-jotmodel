fdc63d2888ed9166fa55390455537a5e9bf0e0e0
Mike Davies <isofarro2@gmail.com>
2014-06-08 08:58:45 +0100

    Add content_envelope view to refactor multiple-joins into the database.

--- etc/schema/content-envelope-models.sql
+++ etc/schema/content-envelope-models.sql
@@ -68,3 +68,27 @@ CREATE TABLE IF NOT EXISTS `content` (
 CREATE INDEX `content_1` ON `content` ( _status_id, slug );
 CREATE INDEX `content_2` ON `content` ( _content_id );
 
+--
+-- A one-table view of the four relational content envelope tables
+--
+CREATE VIEW `content_envelope` AS
+SELECT
+    content._id           AS _envelope_id,
+    content._content_id   AS _content_id,
+    content._status_id    AS _status_id,
+    content._model_id     AS _model_id,
+    content_types._id     AS _types_id,
+    content_status.slug   AS status,
+    content_models.slug   AS model,
+    content_types.slug    AS type,
+    content.slug          AS slug,
+    content.title         AS title,
+    content.excerpt       AS excerpt,
+    content.permalink     AS permalink,
+    content.imageTemplate AS imageTemplate,
+    content.dateAdded     AS dateAdded,
+    content.dateUpdated   AS dateUpdated
+FROM `content`
+LEFT JOIN `content_status` ON content._status_id = content_status._id
+LEFT JOIN `content_models` ON content._model_id = content_models._id
+LEFT JOIN `content_types`  ON content_models._type_id = content_types._id;
