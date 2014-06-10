c1f02fd7650cf670e363a8313f81ed04e0f595ef
Mike Davies <isofarro2@gmail.com>
2014-06-10 20:41:15 +0100

    Add SQL metadata to Author

--- lib/JotBooks/Models/Author.php
+++ lib/JotBooks/Models/Author.php
@@ -3,6 +3,18 @@ namespace JotBooks\Models;
 
 class Author
 {
+    public static $MODEL='author';
+    public static $SQL_FRAGMENTS = array(
+        'queries' => array(),
+        'hydrate' => array(),
+        'joins'   => array()
+    );
+    public static $SQL_FIELDS = array(
+        'id'   => '',
+        'name' => '',
+        'slug' => ''
+    );
+
 
     protected $id;
 
