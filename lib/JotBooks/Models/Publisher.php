b3019fc57afc73805576810306820be20a99e77d
Mike Davies <isofarro2@gmail.com>
2014-06-10 20:41:27 +0100

    Add SQL metadata to Publisher

--- lib/JotBooks/Models/Publisher.php
+++ lib/JotBooks/Models/Publisher.php
@@ -3,6 +3,18 @@ namespace JotBooks\Models;
 
 class Publisher
 {
+    public static $MODEL='publisher';
+    public static $SQL_FRAGMENTS = array(
+        'queries' => array(),
+        'hydrate' => array(),
+        'joins'   => array()
+    );
+    public static $SQL_FIELDS = array(
+        'id'   => '',
+        'name' => '',
+        'slug' => ''
+    );
+
     protected $id;
 
     public $name;
