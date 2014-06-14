61a32452a5e3a0ddc0bd84f92227bfebee60f22d
Mike Davies <isofarro2@gmail.com>
2014-06-14 14:50:41 +0100

    Allow envelope properties when Model doesn't have methods. Just about usable for Twig templates.

--- lib/JotModel/Models/EnvelopeContent.php
+++ lib/JotModel/Models/EnvelopeContent.php
@@ -1,6 +1,46 @@
 <?php
 namespace JotModel\Models;
 
-class EnvelopeContent
+abstract class EnvelopeContent
 {
+    protected $envelope;
+
+
+    public function setEnvelope($envelope)
+    {
+        $this->envelope = $envelope;
+    }
+
+
+    public function getEnvelope()
+    {
+        return $this->envelope;
+    }
+
+
+    public function hasEnvelope()
+    {
+        return !empty($this->envelope);
+    }
+
+
+    public function getSlug()
+    {
+        return $this->getOverlaidProperty('slug');
+    }
+
+    public function getTitle()
+    {
+        return $this->getOverlaidProperty('title');
+    }
+
+
+    protected function getOverlaidProperty($propName)
+    {
+        if (property_exists($this, $propName)) {
+            return $this->{$propName};
+        } elseif (property_exists($this->envelope, $propName)) {
+            return $this->envelope->{$propName};
+        }
+    }
 }
