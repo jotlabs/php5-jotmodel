54bd4a644090d981fad0f60ed694a601be28c7c0
Mike Davies <isofarro2@gmail.com>
2014-06-15 22:01:38 +0100

    Example model: Video, extends ContentEnvelope

---
+++ lib/JotModelExamples/Models/Video.php
@@ -0,0 +1,66 @@
+<?php
+namespace JotModelExamples\Models;
+
+use JotModel\Models\ContentEnvelope;
+
+class Video extends ContentEnvelope
+{
+    # TODO: Abstract these static properties into a schema class or config object
+    public static $MODEL = 'videos';
+
+    public static $SQL_FRAGMENTS = array(
+        'queries' => array(
+            'getById' => 'SELECT {fieldList} FROM `videos` WHERE _id=:id;'
+        ),
+        'joins' => array(),
+        'hydrate' => array(
+            'video_blobs' => array(
+                'modelClass' => 'LotsOfYoga\Model\Models\VideoBlob',
+                'where'      => array('videoId' => 'videoId'),
+                'properties' => array('videoId' => 'id')
+            ),
+            'video_images' => array(
+                'modelClass' => 'LotsOfYoga\Model\Models\VideoImage',
+                'where'      => array('videoId' => 'videoId'),
+                'properties' => array('videoId' => 'id')
+            )
+        )
+    );
+
+    public static $SQL_FIELDS = array(
+        'id'            => '_id',
+        'sourceId'      => '',
+        'sourceUrl'     => '',
+        'posterName'    => '',
+        'posterProfile' => '',
+        'datePosted'    => '',
+        'duration'      => '',
+        'numberViews'   => '',
+
+        // Sub-models
+        'blobs'         => '@video_blobs',
+        'images'        => '@video_images'
+    );
+
+
+    protected $id;
+
+    public $sourceId;
+    public $sourceUrl;
+
+    public $posterName;
+    public $posterProfile;
+    public $datePosted;
+
+    public $duration;
+    public $numberViews;
+
+    public $blobs;
+    public $images;
+
+
+    public function getId()
+    {
+        return $this->id;
+    }
+}
