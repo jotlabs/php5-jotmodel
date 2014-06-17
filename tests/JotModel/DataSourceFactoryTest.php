62af65d2f3a3dd8c277040c9e5514e023f734fc2
Mike Davies <isofarro2@gmail.com>
2014-06-17 09:12:10 +0100

    test config-specified datasources work

--- tests/JotModel/DataSourceFactoryTest.php
+++ tests/JotModel/DataSourceFactoryTest.php
@@ -24,9 +24,36 @@ class DataSourceFactoryTest extends PHPUnit_Framework_TestCase
 
     public function testGetDataSourceReturnsPdoDataSource()
     {
-        $dataSource = $this->factory->getDataSource('');
+        $dataSource = $this->factory->getDataSource('__UNITTEST__');
         $this->assertNotNull($dataSource);
         $this->assertTrue(is_a($dataSource, 'JotModel\DataSource'));
         $this->assertTrue(is_a($dataSource, 'JotModel\DataSources\PdoDataSource'));
+    }
+
+
+    public function testGetNamedDataSourceReturnsPdoDataSource()
+    {
+        $dsConfig = array(
+            'testDs' => array(
+                'type' => 'PDO',
+                'dsn'  => 'sqlite::memory:'
+            )
+        );
+        $dsConfig = json_decode(json_encode($dsConfig));
+
+
+        $this->factory->setConfig($dsConfig);
+        $dataSource = $this->factory->getDataSource('testDs');
+        $this->assertNotNull($dataSource);
+        $this->assertTrue(is_a($dataSource, 'JotModel\DataSource'));
+        $this->assertTrue(is_a($dataSource, 'JotModel\DataSources\PdoDataSource'));
+
+    }
+
+
+    public function testGetUnknownDataSourceReturnsNull()
+    {
+        $dataSource = $this->factory->getDataSource('NoSuchDataSource');
+        $this->assertNull($dataSource);
     }
 }
