a15918f52df78e8c564e6d2cf824b14a66101cb9
Mike Davies <isofarro2@gmail.com>
2014-06-23 14:41:42 +0100

    Tag model

---
+++ lib/JotModel/Models/Tag.php
@@ -0,0 +1,34 @@
+<?php
+namespace JotModel\Models;
+
+class Tag
+{
+    public static $MODEL = 'tags';
+
+    public static $SQL_FRAGMENTS = array(
+        'queries' => array(),
+        'hydrate' => array(),
+        'joins'   => array()
+    );
+
+    public static $SQL_FIELDS = array(
+        'tagId'          => '',
+        'tag'            => '',
+        'name'           => '',
+        'collectionId'   => '',
+        'collectionSlug' => '',
+        'collectionName' => '',
+        'dateAdded'      => '',
+    );
+
+    protected $envelopeId;
+    protected $tagId;
+    public $tag;
+    public $name;
+
+    protected $collectionId;
+    public $collectionSlug;
+    public $collectionName;
+
+    public $dateAdded;
+}
