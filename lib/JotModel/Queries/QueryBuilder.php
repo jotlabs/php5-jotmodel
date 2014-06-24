f9fd02e09d0bd4a1a1e638a425aa94effd86b75d
Mike Davies <isofarro2@gmail.com>
2014-06-24 18:13:22 +0100

    QueryBuilder method for accepting range values

--- lib/JotModel/Queries/QueryBuilder.php
+++ lib/JotModel/Queries/QueryBuilder.php
@@ -9,6 +9,9 @@ class QueryBuilder
     protected $queryName;
     protected $filters = array();
 
+    protected $rangeStart;
+    protected $rangeLength;
+
 
     public function build()
     {
@@ -16,6 +19,11 @@ class QueryBuilder
         $query->setModelClass($this->modelClass);
         $query->setQueryName($this->queryName);
         $query->setFilters($this->filters);
+
+        if ($this->rangeStart || $this->rangeLength) {
+            $query->setRange($this->rangeStart, $this->rangeLength);
+        }
+
         return $query;
     }
 
@@ -37,5 +45,14 @@ class QueryBuilder
     public function filter($property, $value)
     {
         $this->filters[$property] = $value;
+        return $this;
+    }
+
+
+    public function setRange($start, $length)
+    {
+        $this->rangeStart  = intval($start);
+        $this->rangeLength = intval($length);
+        return $this;
     }
 }
