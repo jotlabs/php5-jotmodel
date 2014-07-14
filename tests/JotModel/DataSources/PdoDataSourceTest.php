4740803ce8bf71f41cc714193775ffa4ad91ca47
Mike Davies <isofarro2@gmail.com>
2014-07-14 07:04:51 +0100

    Create and populate in-memory database in preparation for testing the save method

--- tests/JotModel/DataSources/PdoDataSourceTest.php
+++ tests/JotModel/DataSources/PdoDataSourceTest.php
@@ -6,13 +6,17 @@ use PDO;
 
 class PdoDataSourceTest extends PHPUnit_Framework_TestCase
 {
+    public static $dsn        = 'sqlite::memory:';
+    //public static $dsn        = 'sqlite:tmp/testdb.db';
+    public static $schemaFile = '/var/testdata/unittest-schema.sql';
+    public static $dataFile   = '/var/testdata/unittest-data.sql';
     protected $dataSource;
 
 
     public function setUp()
     {
-        $pdoDb = new PDO('sqlite::memory:');
-        $this->dataSource = new PdoDataSource($pdoDb);
+        $db = $this->createDb();
+        $this->dataSource = new PdoDataSource($db);
     }
 
     public function testPdoDataSourceExists()
@@ -30,5 +34,19 @@ class PdoDataSourceTest extends PHPUnit_Framework_TestCase
         $db = (object) null;
         $dataSource = new PdoDataSource($db);
         $this->assertFalse(true);
+    }
+
+
+    protected function createDb()
+    {
+        $db = new PDO(self::$dsn);
+
+        $sql = file_get_contents(PROJECT_ROOT . self::$schemaFile);
+        $db->exec($sql);
+
+        $sql = file_get_contents(PROJECT_ROOT . self::$dataFile);
+        $db->exec($sql);
+
+        return $db;
     }
 }
