61b8521106412d1d13ff8968d79e1fe46dc56722
Mike Davies <isofarro2@gmail.com>
2014-07-14 07:18:03 +0100

    test findOne and find return Model and array of Models

--- tests/JotModel/DataSources/PdoDataSourceTest.php
+++ tests/JotModel/DataSources/PdoDataSourceTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace JotModel\DataSources;
 
+use JotModel\Queries\QueryBuilder;
 use PHPUnit_Framework_TestCase;
 use PDO;
 
@@ -34,6 +35,49 @@ class PdoDataSourceTest extends PHPUnit_Framework_TestCase
         $db = (object) null;
         $dataSource = new PdoDataSource($db);
         $this->assertFalse(true);
+    }
+
+
+    public function testFindOneReturnsModel()
+    {
+        $slug    = 'the-ashtanga-primary-series';
+        $builder = new QueryBuilder();
+        $builder
+            ->setModelClass('JotModelExamples\Models\Video')
+            ->setQueryName('getBySlug')
+            ->filter('slug', $slug);
+
+        $query = $builder->build();
+        $video = $this->dataSource->findOne($query);
+
+        $this->assertNotNull($video);
+        $this->assertTrue(is_a($video, 'JotModel\Models\ContentEnvelope'));
+        $this->assertTrue(is_a($video, 'JotModelExamples\Models\Video'));
+        $this->assertTrue(intval($video->getEnvelopeId()) > 0);
+    }
+
+
+    public function testFindReturnsArrayOfModels()
+    {
+        $category = 'ashtanga';
+        $builder  = new QueryBuilder();
+        $builder
+            ->setModelClass('JotModelExamples\Models\Video')
+            ->setQueryName('getByCategory')
+            ->filter('category', $category);
+
+        $query  = $builder->build();
+        $videos = $this->dataSource->find($query);
+
+        $this->assertNotNull($videos);
+        $this->assertTrue(is_array($videos));
+        $this->assertTrue(count($videos) > 0);
+
+        foreach ($videos as $video) {
+            $this->assertTrue(is_a($video, 'JotModel\Models\ContentEnvelope'));
+            $this->assertTrue(is_a($video, 'JotModelExamples\Models\Video'));
+            $this->assertTrue(intval($video->getEnvelopeId()) > 0);
+        }
     }
 
 
