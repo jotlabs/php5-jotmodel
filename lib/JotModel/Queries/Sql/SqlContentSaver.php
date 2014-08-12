833e00bcec83f8f3c0d73e0fcee89d0f771ace9f
Mike Davies <isofarro2@gmail.com>
2014-08-12 07:25:19 +0100

    Save category associated with a content envelope.

--- lib/JotModel/Queries/Sql/SqlContentSaver.php
+++ lib/JotModel/Queries/Sql/SqlContentSaver.php
@@ -3,6 +3,8 @@ namespace JotModel\Queries\Sql;
 
 use JotModel\Queries\Sql\Statements\InsertStatement;
 use JotModel\Queries\QueryBuilder;
+use JotModel\Repositories\ContentRepository;
+use JotModel\Models\ContentEnvelope;
 
 abstract class SqlContentSaver
 {
@@ -12,7 +14,13 @@ abstract class SqlContentSaver
 
     private $typeModels  = array();
     private $contQueries = array(
-        'saveEnvelope'   => 'INSERT INTO `content` VALUES(NULL, :statusId, :modelId, :contentId, :slug, :title, :excerpt, :extra1, :extra2, :permalink, :image, :dateAdded, :dateUpdated, :version, :score);'
+        'saveEnvelope'   => 'INSERT INTO `content` VALUES(NULL, :statusId, :modelId, :contentId, :slug, :title, :excerpt, :extra1, :extra2, :permalink, :image, :dateAdded, :dateUpdated, :version, :score);',
+
+        // Category Saver
+        'saveEnvelopeCategory' => 'INSERT INTO `content_categories` VALUES(:contentId, :categoryId, :dateAdded);',
+        'saveCategory' => 'INSERT INTO `categories` VALUES(NULL, :collectionId, :slug, :name);'
+
+        // Tag Saver
     );
 
 
@@ -63,8 +71,105 @@ abstract class SqlContentSaver
 
 
         // TODO: Save decorators
+        $envelopeId = $this->getEnvelopeIdBySlug($model->slug);
+        if ($envelopeId) {
+            $decorators   = $model::$DECORATORS;
+            $methodPrefix = 'saveContent';
+
+            foreach ($decorators as $decorator) {
+                if (!empty($model->{$decorator})) {
+                    $method = $methodPrefix . ucfirst($decorator);
+                    if (method_exists($this, $method)) {
+                        $this->{$method}($envelopeId, $model->{$decorator});
+                    }
+
+                }
+            }
+        }
+
 
         return $response;
+    }
+
+
+    protected function saveContentTags($envelopeId, $modelTags)
+    {
+        //echo "Saving Content Tags for {$envelopeId}\n";
+    }
+
+
+    protected function saveContentCategories($envelopeId, $modelCategories)
+    {
+        $response = false;
+        $now = date('c');
+
+        $stmName = 'saveEnvelopeCategory';
+        $insert  = new InsertStatement();
+        $insert->setQueryName($stmName);
+        $insert->setStatement($this->contQueries['saveEnvelopeCategory']);
+
+        foreach ($modelCategories as $slug => $title) {
+            $category   = (object) array(
+                'slug'         => $slug,
+                'title'        => $title
+            );
+
+            $categoryId = $this->saveCategory($category);
+
+            if ($categoryId) {
+                $params = array(
+                    ':contentId'  => $envelopeId,
+                    ':categoryId' => $categoryId,
+                    ':dateAdded'  => $now
+                );
+
+                $response = $this->dataSource->insert($insert, $params);
+            }
+        }
+
+        return $response;
+    }
+
+
+    protected function saveCategory($category)
+    {
+        $categoryId = $this->getCategoryBySlug($category->slug);
+
+        if (!$categoryId) {
+            $stmName = 'saveCategory';
+            $insert  = new InsertStatement();
+            $insert->setQueryName($stmName);
+            $insert->setStatement($this->contQueries['saveCategory']);
+
+            $params = array(
+                ':slug'         => $category->slug,
+                ':name'         => $category->title,
+                ':collectionId' => 1
+            );
+
+            $response = $this->dataSource->insert($insert, $params);
+
+            if ($response) {
+                $categoryId = $this->getCategoryBySlug($category->slug);
+            }
+        }
+
+        return $categoryId;
+    }
+
+
+    protected function getCategoryBySlug($categorySlug)
+    {
+        $builder = new QueryBuilder();
+        $builder
+            ->setModelClass('JotModel\Models\Category')
+            ->setQueryName('categoryBySlug')
+            ->filter('slug', $categorySlug);
+
+        $query    = $builder->build();
+        $category = $this->dataSource->findOne($query, false);
+
+        return $category ? $category->id : null;
     }
 
 
@@ -92,5 +197,28 @@ abstract class SqlContentSaver
         }
 
         return $typeModel;
+    }
+
+
+    protected function getEnvelopeIdBySlug($slug)
+    {
+        $content    = $this->getContentBySlug($slug);
+        $envelopeId = $content->getEnvelopeId();
+        return $envelopeId;
+    }
+
+
+    protected function getContentBySlug($slug)
+    {
+        $builder = new QueryBuilder();
+        $builder
+            ->setModelClass('JotModel\Models\ContentEnvelope')
+            ->setQueryName('getBySlug')
+            ->filter('slug', $slug);
+
+        $query   = $builder->build();
+        $content = $this->dataSource->findOne($query);
+
+        return $content;
     }
 }
